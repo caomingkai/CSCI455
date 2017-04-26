361951558072c703f913243debd0692c920e7325
caomingkai <caomingkai@outlook.com>
2017-04-26 02:25:05 -0700

    polynomial calculation: arraylist/ assert usage/

---
+++ PA2/AssertTester.java
@@ -0,0 +1,21 @@
+/**
+
+   Test the ASSERT in Term Class
+
+ */
+public class AssertTester{
+
+	public static void main(String[] args){
+
+		
+		Term term_Y = new Term( 2.3 , 5 );
+    	Term term_N=  new Term( 2.3 , -2 );
+
+    	while(true){
+    		System.out.println( term_Y.toString() );
+    	System.out.println( term_N.toString() );
+    	}
+    			
+	}
+
+}
